4d8ab75c82dc13a17517d2934d873093f320a68d
Brock Chelle <Brocklchelle@gmail.com>
2021-01-21 15:32:23 -0700

    Chapter 3.4: "Comments"

---
+++ functions/comments/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    // You can put comments on a separate line (preferred)
+    println!("Hello, world!"); // You can put comments at the end of lines too (not preferred)
+
+    // This
+    // is
+    // how
+    // you
+    // should
+    // write
+    // multi
+    // line
+    // comments
+    // in
+    // rust
+
+    /*
+    Though
+    this
+    works
+    too,
+    but
+    the
+    other
+    way
+    is
+    most
+    common
+    */
+}
