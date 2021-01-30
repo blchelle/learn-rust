ae6e2efee0a8dc4c59ffc26eb5c5aefe23a17b4a
Brock Chelle <Brocklchelle@gmail.com>
2021-01-30 11:56:01 -0700

    Chapter 7.2: "Defining Modules to  Control Scope and Privacy"

---
+++ chapter-7/restaurant/src/lib.rs
@@ -0,0 +1,26 @@
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+// The above code creates a module tree like so
+// crate
+//  └── front_of_house
+//      ├── hosting
+//      │   ├── add_to_waitlist
+//      │   └── seat_at_table
+//      └── serving
+//          ├── take_order
+//          ├── serve_order
+//          └── take_payment
