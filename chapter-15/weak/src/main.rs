3d66b2e2ae5e632bab5aad64d58ac615f308c97a
Brock Chelle <Brocklchelle@gmail.com>
2021-03-03 21:44:29 -0700

    Chapter 15.6: Reference Cycles Can Leak Memory

---
+++ chapter-15/weak/src/main.rs
@@ -0,0 +1,83 @@
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
+}
+
+fn main() {
+    oh_no_a_cycle();
+    println!();
+
+    tree_things();
+    println!();
+}
+
+// This function creates two cons nodes
+// a is initialized with a value 5 and a tail of nil
+// b is initialized with a value 10 and with a as its tail
+// then a is modified to have b as its tail which creates a cyclic structure
+fn oh_no_a_cycle() {
+    println!("===== OH NO A CYCLE =====");
+
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // Uncomment the next line to see that we have a cycle;
+    // it will overflow the stack
+    // println!("a next item = {:?}", a.tail());
+}
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
+fn tree_things() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!("The leafs parent is {:?}", leaf.parent.borrow().upgrade());
+
+    let branch = Rc::new(Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+
+    // Makes branch leafs parent
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+    println!("The leafs parent is {:?}", leaf.parent.borrow().upgrade());
+}
